c11be812cf45280451e09eb6c534ce95d1a83864
devikavn22 <devika1997eee@gmail.com>
2023-11-21 15:24:18 +0530

    Added comments

--- JSDSA/JS-LeetCode-Reverse-LinkedList/iterative.js
+++ JSDSA/JS-LeetCode-Reverse-LinkedList/iterative.js
@@ -9,16 +9,24 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-    let prev = null;
-     let current = head;
- 
-     while (current !== null) {
-         let nextTemp = current.next;
-         current.next = prev;
-         prev = current;
-         current = nextTemp;
-     }
- 
-     return prev;
- };
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+
+  while (current !== null) {
+    let nextTemp = current.next;
+    current.next = prev;
+    prev = current;
+    current = nextTemp;
+  }
+
+  return prev;
+};
+
+/* 
+ Time Complexity: O(n) - Although the recursive solution looks like it might be O(n^2) due to the 
+ recursive call within the loop, the recursive calls are tail-recursive. In many JavaScript environments, 
+ the tail call is optimized, effectively making it O(n).
+Space Complexity: O(n) - The space complexity is O(n) due to the recursive call stack. In the worst case, 
+the maximum depth of the call stack would be n (the number of nodes in the linked list)
+ */
--- JSDSA/JS-LeetCode-Reverse-LinkedList/recursive.js
+++ JSDSA/JS-LeetCode-Reverse-LinkedList/recursive.js
@@ -9,14 +9,20 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-    if (head === null || head.next === null) {
-           return head;
-       }
+var reverseList = function (head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+
+  let reversedList = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+
+  return reversedList;
+};
+
+/* 
+Time Complexity: O(n) - where n is the number of nodes in the linked list. We iterate through each node once.
+Space Complexity: O(1) - We use a constant amount of extra space regardless of the size of the input.
    
-       let reversedList = reverseList(head.next);
-       head.next.next = head;
-       head.next = null;
-   
-       return reversedList;
-   };
+   */
